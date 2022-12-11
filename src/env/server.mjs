a8220ab6995587e7e44de4dc2ba0c6a4ce247757
Talha-Bayansar <talha.bayansar@hotmail.com>
2022-12-11 16:08:09 +0100

    commenting lines in files in env folder

--- src/env/server.mjs
+++ src/env/server.mjs
@@ -10,8 +10,8 @@ const _serverEnv = serverSchema.safeParse(process.env);
 
 if (!_serverEnv.success) {
   console.error(
-    "❌ Invalid environment variables:\n",
-    ...formatErrors(_serverEnv.error.format()),
+    "❌ Invalid environment variables:\n"
+    // ...formatErrors(_serverEnv.error.format()),
   );
   throw new Error("Invalid environment variables");
 }
